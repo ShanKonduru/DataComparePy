43298c133e6a82155574111f050bc9b3c7e66f87
ShanKonduru <ShanKonduru@gmail.com>
2024-06-22 16:25:18 -0600

    working Prototype

--- runexl2exl.py
+++ runexl2exl.py
@@ -6,29 +6,6 @@ from ExcelDataComparator import ExcelDataComparator
 if __name__ == "__main__":
     # Load configuration from JSON file
     config_loader = ConfigLoader('config.json')
-    config = config_loader.get_config()
-    
-    source_file_path = config["source_file_path"]
-    target_file_path = config["target_file_path"]
-    sheet_name = config["sheet_name"]
-    src_query = config["src_query"]
-    dest_query = config["dest_query"]
-    
-    # Create an instance of ExcelDataComparator
-    data_comparator = ExcelDataComparator(source_file_path, target_file_path, sheet_name)
-    
-    # Compare Excel with Excel using SQL query
-    result_excel_excel = data_comparator.compare_excel_with_excel(src_query, dest_query)
-    print(result_excel_excel)
-
-import pandas as pd
-from pandasql import sqldf
-from ConfigLoader import ConfigLoader
-from ExcelDataComparator import ExcelDataComparator
-
-if __name__ == "__main__":
-    # Load configuration from JSON file
-    config_loader = ConfigLoader('config.json')
     
     # Get all dataset names (IDs)
     dataset_ids = config_loader.get_all_dataset_names()
